e6f64e35f596554c2ceeb8801050447d0499191c
Andrew Weihe <andrewy03@gmail.com>
2020-02-10 15:15:04 -0600

    heroku upload

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
-var mysql = require("mysql");
-connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Pistons03",
-  database: "burgers_db"
-});
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "burgers_db"
+    });
+  }
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
